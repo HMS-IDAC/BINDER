9a9d041c624bf0c04c5eceec402468f019fba164
Chintapalli <sc648@idac-srv2.med.harvard.edu>
2020-04-23 10:49:54 -0400

    Data generator for traininga nd testing autoencoder model

--- autoencoder/src/data_gen.py
+++ autoencoder/src/data_gen.py
@@ -52,17 +52,37 @@ X = Manipulator('./src/manipulations.yaml')
 
 ## Sequence genertor (use multiprocessing=True and multiple threads for faster GPU computation)
 class DataGenerator(tensorflow.keras.utils.Sequence):
-    """Generates positive and negative pairs if prob_neg_pos > 0, else geneartes only positve pairs
+    
+    """
+    Generates positive and negative pairs if prob_neg_pos > 0, 
+    else geneartes only positve pairs
+       
        Input - path to directory with images
-       Ouput - Batch of Image pairs and labels: 1 = 'same'(postive pair) | 0 = 'different' (negative_pair)
+       Ouput - Batch of Image pairs and labels: 
+               1 = 'same'(postive pair) | 0 = 'different' (negative_pair) 
+       
+       returns - {dict1- input to model} , {dict2- Ground Truth for supervised-learning}  
+       dict1:
+           'Input_l' - anchor image
+           'Input_r' - same src manipulated or random different img
+       dict2:
+           'output_1' - same as 'Input_l' (for training autoencoder base network)
+           'output_2' - same as 'Input_r' (for training autoencoder base network)
+           'l2_norm_d' - batch labels (0 or 1 for different or same respectively)
+           'dense_embedding - batch labels (0 or 1 for different or same respectively)
+       
        'Initialization'
            dir_path: path to directory
            batch_size: batch size
-           input_size: image size
-           n_channels: number of channels (1 or 3)
+           input_size: image size (e.g. = 128, i.e. height or width b/c aspect ratio =1) 
+           n_channels: number of channels (= 1 or 3) 
            prob_neg_pos:probability of negative samples [0,1]
-           dataset: train/valid if directory contains subfolders choose valid else choose train
+           dataset: 'train' / 'valid'
+                    if directory has img files choose 'train', 
+                    if directory has subfolders with img files choose'valid'
+                  
     """
+    
     def __init__(self, dir_path, batch_size=1, input_size=128, n_channels=1, prob_neg_pos=0.5, dataset='train',
                  shuffle=True,seed_img=None):
         'Initialization'
@@ -366,6 +386,35 @@ def test_imageconvert_generator_hardneg(dir_path,batch_size=2,img_size=128,n_cha
             yield {'Input_l':img_anchor,'Input_r':img_twin} , {'l2_norm_d':batch_labels}
 
 class test_gen():
+    """
+    Generates positive and negative pairs of images 
+    for testing/validating the model performance
+       
+       Input - path to directory with images
+       Ouput - Batch of Image pairs and labels: 
+               1 = 'same'(postive pair) | 0 = 'different' (negative_pair) 
+       
+       returns - {dict1 - input to model} , {dict2 - Ground Truth labels}  
+       
+       dict1:
+           'Input_l' - anchor image
+           'Input_r' - same src manipulated or random different img
+       dict2:
+           'l2_norm_d' - batch labels (0 or 1 for different or same respectively)
+       
+       'Initialization'
+           dir_path: path to directory
+           test_dataset_name: PUBPEER | MFND | BINDER 
+           neg_selection: 
+           batch_size: batch size
+           input_size: image size (e.g. = 128, i.e. height or width b/c aspect ratio =1) 
+           n_channels: number of channels (= 1 or 3) 
+           prob_neg_pos:probability of negative samples [0,1]
+           dataset: 'train' / 'valid'
+                    if directory has img files choose 'train', 
+                    if directory has subfolders with img files choose'valid'
+                  
+    """
     def __init__(self, dir_path, test_dataset_name, neg_selection,  batch_size, n_channels, img_size=128, seed_dir=None,seed_img=None):
         self.dir_path=dir_path
         self.test_dataset_name=test_dataset_name
